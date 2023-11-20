5e002af68b8668bdc694d3bb3c49fa75f1ce9916
DhanushK3 <142695470+DhanushK3@users.noreply.github.com>
2023-11-19 23:26:59 -0500

    Update AssasmentWeek4Q16Application.java

--- AssasmentWeek_4-order-microservises/src/main/java/com/infubuzz/app/AssasmentWeek4Q16Application.java
+++ AssasmentWeek_4-order-microservises/src/main/java/com/infubuzz/app/AssasmentWeek4Q16Application.java
@@ -1,13 +1,34 @@
 package com.infubuzz.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-public class AssasmentWeek4Q16Application {
+@ComponentScan({"com.infybuzz.controller", "com.infybuzz.service"})
+@EntityScan("com.infybuzz.entity")
+@EnableJpaRepositories("com.infybuzz.repository")
+public class AssasmentWeek4Q16Application { 
+	
+	@Value("${customer.service.url}")
+	private String customerServiceUrl;
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssasmentWeek4Q16Application.class, args);
+		SpringApplication.run(AssasmentWeek4Q16Application.class, args); 
+	}
+	
+	@Bean
+	public WebClient webClient() {
+		WebClient webClient = WebClient.builder()
+				.baseUrl(customerServiceUrl)
+				.build();
+
+		return webClient;
 	}
 
 }
