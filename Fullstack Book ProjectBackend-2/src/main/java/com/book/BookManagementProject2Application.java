0ac7d38770d1b9361ca35992a5239f41595d87ca
DhanushK3 <142695470+DhanushK3@users.noreply.github.com>
2023-11-27 18:34:07 -0500

    Add files via upload

---
+++ Fullstack Book ProjectBackend-2/src/main/java/com/book/BookManagementProject2Application.java
@@ -0,0 +1,13 @@
+package com.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementProject2Application.class, args);
+	}
+
+}
---
+++ Fullstack Book ProjectBackend-2/src/main/java/com/book/entity/Book.java
@@ -0,0 +1,92 @@
+package com.book.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "book_name")
+    private String bookName;
+
+    private String author;
+
+    private String category;
+
+    private int qty;
+
+    private double price;
+
+    // Constructors, getters, and setters
+
+    public Book() {
+        // Default constructor
+    }
+
+    public Book(String bookName, String author, String category, int qty, double price) {
+        this.bookName = bookName;
+        this.author = author;
+        this.category = category;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
